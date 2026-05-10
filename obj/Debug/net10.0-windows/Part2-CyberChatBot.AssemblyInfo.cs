c265801599e50a90141523422519e55b0ad5564b
Bongani Dlamini <st10451243@myemeris.edu.za>
2026-05-10 20:18:28 +0200

    Added the remaining classes that need to be added, which are from my part 1

--- obj/Debug/net10.0-windows/Part2-CyberChatBot.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Part2-CyberChatBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Part2-CyberChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29c9e2830eacae4f1064fea8a487138ee7f62fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Part2-CyberChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Part2-CyberChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
